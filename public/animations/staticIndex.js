c197395714da5cfe9aacce46998e426f9e9028da
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2019-08-13 19:47:06 +0530

    navbar-brand changed with logo image.

--- public/animations/staticIndex.js
+++ public/animations/staticIndex.js
@@ -77,4 +77,60 @@ $(document).ready(function () {
             });
         }, 2000
     );
+
+    document.getElementById('home_header').addEventListener('mouseover', () => {
+
+        let home = document.getElementById('home_header');
+
+        home.style.borderBottom = '2px solid #76D7C4';
+    });
+
+    document.getElementById('home_header').addEventListener('mouseout', () => {
+
+        let home = document.getElementById('home_header');
+
+        home.style.borderBottom = 'none';
+    });
+
+    document.getElementById('about_header').addEventListener('mouseover', () => {
+
+        let home = document.getElementById('about_header');
+
+        home.style.borderBottom = '2px solid #76D7C4';
+    });
+
+    document.getElementById('about_header').addEventListener('mouseout', () => {
+
+        let home = document.getElementById('about_header');
+
+        home.style.borderBottom = 'none';
+    });
+
+    document.getElementById('post_header').addEventListener('mouseover', () => {
+
+        let home = document.getElementById('post_header');
+
+        home.style.borderBottom = '2px solid #76D7C4';
+    });
+
+    document.getElementById('post_header').addEventListener('mouseout', () => {
+
+        let home = document.getElementById('post_header');
+
+        home.style.borderBottom = 'none';
+    });
+
+    document.getElementById('contact_header').addEventListener('mouseover', () => {
+
+        let home = document.getElementById('contact_header');
+
+        home.style.borderBottom = '2px solid #76D7C4';
+    });
+
+    document.getElementById('contact_header').addEventListener('mouseout', () => {
+
+        let home = document.getElementById('contact_header');
+
+        home.style.borderBottom = 'none';
+    });
 });

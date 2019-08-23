600a4783ea114bf89cb059c49647ad6cdd2580f1
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2019-08-23 17:42:04 +0530

    Added the js file for upload page

---
+++ public/animations/dynamicPages/uploadImage.js
@@ -0,0 +1,29 @@
+function updateInput() {
+    $(".custom-file-input").on("change", function () {
+        var fileName = $(this).val().split("\\").pop();
+        $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+    });
+}
+
+function flip() {
+    let card = document.getElementById('main_card');
+
+    anime({
+        targets: card,
+        scale: [0, 1],
+        rotateY: 360,
+        duration: function (el, i, l) {
+            return 6000 + 300 * i;
+        },
+        elasticity: function (el, i, l) {
+            return 200 + 100 * i;
+        },
+        delay: function (el, i, l) {
+            return 700 * i;
+        }
+    })
+}
+
+//function call statements
+updateInput();
+flip();
